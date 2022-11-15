d8318f20949d13cf87bfbd6adc9d6c210fd64ebb
jauhc <paperslove420@gmail.com>
2022-11-15 07:00:36 +0200

    brainrot is real

--- src/ramble.nim
+++ src/ramble.nim
@@ -4,29 +4,13 @@ import utils
 import strformat
 import strutils
 
-# TODO
-#  secure
-#  or just use some framework like jester
+const
+    areyoulost = readFile("res/lost.png")
+    devmode* = true
 
-# 1 gb = 1024 * 1024 * 1024
-const # holy fuck this became cluttered but const is comptime so its ok i guess
-    devmode = true
-    host = "localhost"
-    port = 8080
-
-    isSecure = false # todo, TLS/SSL or whatever its called today
-
-    protocol: string = if isSecure: "https" else: "http"
-    endpoint = case port
-        of 80, 443: &"{protocol}://{host}/"
-        else: &"{protocol}://{host}:{port}/"
-
-    one_meg = 1024 * 1024 # 1 mb
-    storage_capacity = one_meg * (256)  # mb
-#const storage_size = 1024 * 1024 * 1024 # 128 mb
-
-#import base64
-const areyoulost = readFile("res/lost.png")
+var 
+    iStorage_capacity: int = 1
+    bSecure: bool = false
 
 import std/tables
 var storage {.threadvar.}: OrderedTableRef[int64, string]
@@ -36,63 +20,45 @@ proc storage_size(): int =
     var t = 0
     for v in storage.values:
         t = t + len(v)
-        echo "[scan] found file with size ", len(v)
+        echo "[scan] found file with size ", utils.toReadable(len(v))
     return t
 
 proc storagedump() =
     var size = storage_size()
     utils.log("storage stats", 3)
-    utils.log(&"storage size: {size}", 3)
+    utils.log(&"storage size: {utils.toReadable(size)}", 3)
     utils.log("contents: ", 3)
     for i in storage.keys:
-        utils.log(&"key: {i}, resource size: {len(storage[i])}", 3)
-    utils.log(&"TOTAL USAGE: {size} / {storage_capacity}", 3)
+        utils.log(&"key: {i}, resource size: {utils.toReadable(len(storage[i]))}", 3)
+    utils.log(&"TOTAL USAGE: {utils.toReadable(size)} / {utils.toReadable(iStorage_capacity)}", 3)
     #echo GC_getStatistics()
 
-proc checkStorage(datalen:int) =
-    #utils.log(&"checking storage..", 3)
-    while storage_size() + datalen > storage_capacity:
+# returns false if too big
+proc checkStorage(datalen:int): bool =
+    utils.log(&"checking storage..", 3)
+
+    if (datalen > iStorage_capacity):
+        utils.log("file too big! :(", 3)
+        return false
+
+    while storage_size() + datalen > iStorage_capacity:
         for k, v in storage.pairs:
             utils.log(&"storage full, removing {k}...", 3)
             storage.del(k)
             break
+    return true
 
-proc createDevResource(res:string, optname:int64 = 0) =
-    let curt = utils.timestampget()
-    checkStorage(len(res))
-    if optname == 0:
+proc createResource(res:string, optname:int64 = 0): string =
+    let curt = (if optname == 0: utils.timestampget() else: optname)
+    if checkStorage(len(res)):
         storage[curt] = res
     else:
-        storage[optname] = res
+        return "fatass"
 
-    log(&"(+) resource added, name: {optname}, len: {len(res)}",3) #\
-    #log(&"(+) resource: {storage[curt]}",3) # prints entire image as text (dont do this)
-    #storagedump()
-
-proc createResource(res:string): string =
-    let curt = utils.timestampget()
-    checkStorage(len(res))
-    storage[curt] = res
-
-    log(&"(+) resource added, name: {curt}, len: {len(res)}",3) #\
+    log(&"(+) resource added, name: {curt}, len: {utils.toReadable(len(res))}",3) #\
     #log(&"(+) resource: {storage[curt]}",3) # prints entire image as text (dont do this)
     #storagedump()
     return $curt
-
-# temp dev shit dont mind
-when (devmode == true):
-    import std/random
-    randomize()
-    proc tempfile(path:string, optname: int64) =
-        {.used.}
-        let cols = ["red", "blue", "green", "yellow", "purple"]
-        let pick = sample(cols)
-        let rn = rand(100)
-
-        var d = readFile(path)
-        createDevResource(d, optname)
-        writeFile(&"temp/TEMPFILE_{pick}_{rn}", d) # no clue why this
-
 
 proc waitforinput() {.used.} = # not really used, just a relic before async
     storagedump()
@@ -106,7 +72,7 @@ import std/mimetypes
 import std/asyncdispatch
 import std/net
 
-proc serve {.async.} =
+proc serve {.async, gcsafe.} =
     var m = newMimetypes()
     var server = newAsyncHttpServer()
     proc cb(req: Request) {.async, gcsafe.} =
@@ -121,14 +87,16 @@ proc serve {.async.} =
             if req.url.path == "/upload":
                 if req.headers["header_token"] == utils.readCfg("token"):
                     utils.log("wow POSTing works")
-                    if len(req.body) > storage_capacity: # dunno if this is called BEFORE or after entire upload
+                    if len(req.body) > iStorage_capacity: # dunno if this is called BEFORE or after entire upload
                         utils.log("file too big", 3)
                         let headers = {"Content-type": "text/plain; charset=utf-8"}
                         await req.respond(Http200, "fatass", headers.newHttpHeaders())
                         return
                     let filename = createResource(req.body) # could return name if needed for response
                     let headers = {"Content-type": "text/plain; charset=utf-8"}
-                    await req.respond(Http200, &"{endpoint}{filename}", headers.newHttpHeaders())
+                    let endpoint: string = 
+                        (if bSecure: "https" else: "http") & "://" & utils.readCfg("host") & ":" & utils.readCfg("port")
+                    await req.respond(Http200, &"{endpoint}/{filename}", headers.newHttpHeaders())
                     return
                 else: # make more sane or dont
                     await bozofound(req.url.path, req.hostname)
@@ -165,7 +133,7 @@ proc serve {.async.} =
             let headers = {"Content-type": "text/plain; charset=utf-8"}
             await req.respond(Http200, "", headers.newHttpHeaders())
 
-    server.listen(Port(port))#, "0.0.0.0") 
+    server.listen(Port(utils.readCfg("port").parseInt))#, "0.0.0.0") 
 
     while true:
         if server.shouldAcceptRequest():
@@ -175,13 +143,15 @@ proc serve {.async.} =
             # could check for hanging clients here
 
 proc main(): void =
+    log("@main", 3)
     when (devmode == true): # remove
-        tempfile("res/3.jpg", 1)
-        tempfile("res/4.png", 2)
-        tempfile("res/5.jpg", 3)
-        tempfile("res/1.jpg", 4)
-        tempfile("res/2.jpg", 5)
+        discard createResource(readFile("res/3.jpg"), 1)
+        discard createResource(readFile("res/4.png"), 2)
+        discard createResource(readFile("res/5.jpg"), 3)
+        discard createResource(readFile("res/1.jpg"), 4)
+        discard createResource(readFile("res/2.jpg"), 5)
         storagedump()
+        utils.dumpCfg() # false shows token (kinda insecure)
     #waitforinput()
     waitFor serve()
     return
@@ -191,12 +161,9 @@ proc init(): bool =
         log("no config.ini present >:(", 1)
         return false
     utils.reloadCfg()
-    when (devmode == true):
-        if dirExists("temp"):
-            for k in walkDir("temp"):
-                log(&"(!) file found: {k.path}", 3)
-                if not tryRemoveFile(k.path):
-                    return false
+    iStorage_capacity = utils.readCfg("maxsize").parseInt
+    bSecure = utils.readCfg("secure").parseBool
+    utils.log("init successful", 3)
     return true
 
 # aka main()
--- src/utils.nim
+++ src/utils.nim
@@ -1,10 +1,13 @@
 
 import system
 import times
+import strformat
 import strutils
 import httpclient
 import bitops
 import parsecfg
+import unicode
+import sequtils
 
 const time = cpuTime()
 
@@ -21,16 +24,42 @@ proc log*(data:string, mode = 0): void =
     else:
         echo "\x1B[37m(-) ", now, " INFO: \x1B[0m" & data
 
-var g_cfgFile {.threadvar.}: Config
+
+var g_cfgFile* {.threadvar.}: Config
 
 proc readCfg*(val:string): string =
     return g_cfgFile.getSectionValue("", val)
 
 proc reloadCfg*() =
+    log("config reloaded", 3)
     g_cfgFile = loadConfig("config.ini")
+
+proc dumpCfg*(safe:bool = true) =
+    var tempcopy: Config = g_cfgFile
+    if safe:
+        tempcopy.setSectionKey("", "token", "<HIDDEN>")
+            
+    echo g_cfgFile
 
 proc timestampget*(): auto =
     return now().toTime().toUnix()
+
+proc toString(str: seq[char]): string =
+  result = newStringOfCap(len(str))
+  for ch in str:
+    add(result, ch)
+
+proc toReadable*(d:int): string =
+    let s = d.intToStr().reversed().toSeq()
+    # why
+    const b = 1024
+    if s.len > 9:
+        return &"{(d/b/b/b):0.2f} gb"
+    if s.len > 6:
+        return &"{(d/b/b):0.2f} mb"
+    if s.len > 3:
+        return &"{(d/b):0.2f} kb"
+    return $d & "bytes"
 
 # http get
 proc fetch*(w:string): string =
